d9810031e45e4006bea356a6bdeae1396bc15656
Enrique <octepc.97@gmail.com>
2022-09-12 14:08:56 -0500

    manipulated the canva

--- src/game.js
+++ src/game.js
@@ -1,3 +1,16 @@
 const canvas = document.querySelector("#game");
-
 const game = canvas.getContext("2d");
+
+const startGame = () => {
+  window.innerHeight;
+  window.innerWidth;
+
+  // game.fillRect(0, 0, 100, 100);
+  // game.clearRect(29, 20, 10, 10);
+  // game.font = "24px Verdana";
+  // game.fillStyle = "purple";
+  // game.textAlign = "end";
+  // game.fillText("Platzi", 25, 25);
+};
+
+window.addEventListener("DOMContentLoaded", startGame);
