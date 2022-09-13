6f367c91a81bacdf90ce0dd9b769dd861b06bffc
Enrique <octepc.97@gmail.com>
2022-09-12 20:33:21 -0500

    canvas prepared

--- src/game.js
+++ src/game.js
@@ -1,9 +1,10 @@
 const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
 
-const startGame = () => {
-  let canvasSize;
+let canvasSize;
+let elementsSize;
 
+const setCanvasSize = () => {
   if (window.innerHeight > window.innerWidth) {
     canvasSize = window.innerWidth * 0.8;
   } else {
@@ -13,19 +14,22 @@ const startGame = () => {
   canvas.setAttribute("width", canvasSize);
   canvas.setAttribute("height", canvasSize);
 
-  const elementsSize = canvasSize / 10;
+  elementsSize = canvasSize / 10;
 
-  console.log({ canvasSize, elementsSize });
-
-  // window.innerHeight;
-  // window.innerWidth;
-
-  // game.fillRect(0, 0, 100, 100);
-  // game.clearRect(29, 20, 10, 10);
-  // game.font = "24px Verdana";
-  // game.fillStyle = "purple";
-  // game.textAlign = "end";
-  // game.fillText("Platzi", 25, 25);
+  startGame();
 };
 
-window.addEventListener("DOMContentLoaded", startGame);
+const startGame = () => {
+  console.log({ canvasSize, elementsSize });
+
+  game.font = elementsSize + "px Verdana";
+  game.textAlign = "end";
+
+  for (let i = 1; i < 10; i++) {
+    game.fillText(emojis["X"], elementsSize * i, elementsSize);
+    game.fillText(emojis["X"], elementsSize, elementsSize * i);
+  }
+};
+
+window.addEventListener("DOMContentLoaded", setCanvasSize);
+window.addEventListener("resize", setCanvasSize);
